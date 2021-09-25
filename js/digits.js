554aa43761158f00444769a6bffcf692deecd9a6
shijin1984 <40617137+shijin1984@users.noreply.github.com>
2021-09-25 17:56:29 +0800

    Format the init style.

---
+++ js/digits.js
@@ -0,0 +1,9 @@
+$(function() {
+    $("td").css({
+        "width": "33vw",
+        "height": "33vw",
+        "text-align": "center",
+        "vertical-align": "center",
+        "font-size": "28vw"
+    });
+});
