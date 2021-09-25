36671f2038325b837dc0ee629db0fec65e6de29e
shijin1984 <40617137+shijin1984@users.noreply.github.com>
2021-09-25 18:14:16 +0800

    Add a button to shuffle.

--- js/digits.js
+++ js/digits.js
@@ -6,4 +6,18 @@ $(function() {
         "vertical-align": "center",
         "font-size": "28vw"
     });
+
+    $("#shuf").click(function() {
+        var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        for (var i = 0; i < num.length; ++i) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var tmp = num[i];
+            num[i] = num[j];
+            num[j] = tmp;
+        }
+
+        for (var i = 0; i < num.length; ++i) {
+            $("td").eq(i).text(num[i]);
+        }
+    });
 });
