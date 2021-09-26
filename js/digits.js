3c91d558ff2d7c73c6d9ea565fb432ebde29ce0e
shijin1984 <40617137+shijin1984@users.noreply.github.com>
2021-09-26 09:02:26 +0800

    Use CSS to beautify the page instead.

--- js/digits.js
+++ js/digits.js
@@ -1,12 +1,4 @@
 $(function() {
-    $("td").css({
-        "width": "33vw",
-        "height": "33vw",
-        "text-align": "center",
-        "vertical-align": "center",
-        "font-size": "28vw"
-    });
-
     $("#shuf").click(function() {
         var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
         for (var i = 0; i < num.length; ++i) {
