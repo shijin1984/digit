56e2a98842350e3de49d974d4bf49a8fd27bddfd
shijin1984 <40617137+shijin1984@users.noreply.github.com>
2021-09-29 16:09:10 +0800

    Add 2x2 mode.

--- js/digits.js
+++ js/digits.js
@@ -4,12 +4,18 @@ $(function () {
     var fontCollection = [
         "cursive", "emoji", "monospace", "serif", "sans-serif", "Verdana",
         "Comic Sans MS"];
+    var urlParams = new URLSearchParams(window.location.search);
 
-
-    for (var num = 1; num <= 9; ++num) {
-        $("#tgt").append($("<option/>", {text: num, value: num, class: "common"}));
+    var digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    if (urlParams.has("digits")) {
+        var digitStr = urlParams.get("digits");
+        digits = digitStr.split();
     }
-    $("#tgt").val("1");
+
+    digits.forEach(function(num) {
+        $("#tgt").append($("<option/>", {text: num, value: num, class: "tgt_drop"}));
+    });
+    $("#tgt").val(digits[0]);
 
     var getTargetDigit = function() {
         return $("#tgt").val();
@@ -21,7 +27,7 @@ $(function () {
     };
 
     $("#shuf").click(function () {
-        var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        var num = digits.slice();
         for (var i = 0; i < num.length; ++i) {
             var j = Math.floor(Math.random() * (i + 1));
             var tmp = num[i];
