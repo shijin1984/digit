d3e6a3212c16f57f9b286d13d3f56e77cecd5384
shijin1984 <40617137+shijin1984@users.noreply.github.com>
2021-09-29 16:16:36 +0800

    Add a global digits_config for better debugging.

--- js/digits.js
+++ js/digits.js
@@ -1,3 +1,5 @@
+var digits_config {};
+
 $(function () {
     var audio = new Audio("audio/praise.mp3");
     audio.autoplay = false;
@@ -7,15 +9,21 @@ $(function () {
     var urlParams = new URLSearchParams(window.location.search);
 
     var digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
     if (urlParams.has("digits")) {
         var digitStr = urlParams.get("digits");
         digits = digitStr.split();
     }
+    digits_config = {
+        audio: audio,
+        fontCollection: fontCollection,
+        digits: digits
+    };
 
-    digits.forEach(function(num) {
+    digits_config.digits.forEach(function(num) {
         $("#tgt").append($("<option/>", {text: num, value: num, class: "tgt_drop"}));
     });
-    $("#tgt").val(digits[0]);
+    $("#tgt").val(digits_config.digits[0]);
 
     var getTargetDigit = function() {
         return $("#tgt").val();
@@ -27,7 +35,7 @@ $(function () {
     };
 
     $("#shuf").click(function () {
-        var num = digits.slice();
+        var num = digits_config.digits.slice();
         for (var i = 0; i < num.length; ++i) {
             var j = Math.floor(Math.random() * (i + 1));
             var tmp = num[i];
@@ -39,7 +47,7 @@ $(function () {
             $("td").eq(i).text(num[i]);
         }
 
-        $("td").css("font-family", randomSelect(fontCollection));
+        $("td").css("font-family", randomSelect(digits_config.fontCollection));
     });
 
     $("td").click(function () {
